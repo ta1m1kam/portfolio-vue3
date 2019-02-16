b23b90a19e7afbb4d0bf3ee939b75a399ddd92a8
三上大河 <mikamimikami60@gmail.com>
2019-02-16 23:18:21 +0900

    受賞歴追加

--- src/components/partials/Head.vue
+++ src/components/partials/Head.vue
@@ -7,12 +7,17 @@
       <router-link to="/about">About</router-link>
     </el-menu-item>
     <el-menu-item index="3">
-      <router-link to="/works">Works</router-link>
+      <el-dropdown>
+      <span class="el-dropdown-link">
+        <router-link to="/works">Works</router-link><i class="el-icon-arrow-down"></i>
+      </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item><router-link to="/works">Works</router-link></el-dropdown-item>
+          <el-dropdown-item><router-link to="/award">Award</router-link></el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </el-menu-item>
     <el-menu-item index="4">
-      <router-link to="/award">Award</router-link>
-    </el-menu-item>
-    <el-menu-item index="5">
       <router-link to="/contact">Contact</router-link>
     </el-menu-item>
   </el-menu>
